4274a6f93a43cf81f513a6eab071ae18fbaaa423
odekyc <odekyc@gmail.com>
2018-06-02 11:55:08 -0700

    change to odelvacbusiness

--- keys.php
+++ keys.php
@@ -1,10 +1,10 @@
 <?php
  
-   $consumerKey1='MbXni0FJ3BF1huyii8mNYXK3A';
-   $consumerSecret1='M8w1GWryu7EvFpLvEoiN8BWIpc1QUwlaoe9XcX4KHcSQtftz3i';
+   $consumerKey1='CfmRqTRhYZPNJ0RhEZOs7T5tV';
+   $consumerSecret1='mrS3KcHCmKtCmqAQCdlOU84LgGm1qsIpZQJtLS3vB3AxFZuenc';
    
-   $consumerKey2='rxjUk95tq62i2kFcOcnflpgVr';
-   $consumerSecret2='ResUDC55QJuScEhOHSSrIWDQc1FBYwKcw5lLWkriV0TnFfjJa2';
+   $consumerKey2='	npfao3RYXfG3Idx2mwiQd72qJ';
+   $consumerSecret2='pIFub5Msjh7kCCqujorgYpgbSpJFXs8EgIpH4mNvc4olXFgldF';
    
    
    
