66b3a00fe48cc566ce02e3667a2a7f0db1bb23f9
odekyc <odekyc@gmail.com>
2018-07-23 21:27:09 -0700

    final draft as on heroku

--- keys.php
+++ keys.php
@@ -3,7 +3,7 @@
    $consumerKey1='CfmRqTRhYZPNJ0RhEZOs7T5tV';
    $consumerSecret1='mrS3KcHCmKtCmqAQCdlOU84LgGm1qsIpZQJtLS3vB3AxFZuenc';
    
-   $consumerKey2='	npfao3RYXfG3Idx2mwiQd72qJ';
+   $consumerKey2='npfao3RYXfG3Idx2mwiQd72qJ';
    $consumerSecret2='pIFub5Msjh7kCCqujorgYpgbSpJFXs8EgIpH4mNvc4olXFgldF';
    
    
